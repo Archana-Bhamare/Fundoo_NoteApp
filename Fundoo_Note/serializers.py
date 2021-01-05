1975fc0f1a91a7e0edac8f998130c8b1ae577ecc
Archana-Bhamare <archusonar2@gmail.com>
2021-01-05 12:14:01 +0530

    Added : note app serializer

---
+++ Fundoo_Note/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import *
+
+class CreateNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = ['title', 'takeNote']
+
+
+class DisplayNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = '__all__'
+
+
+class RestoreNoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = ['bin']
+
+class LabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Label
+        fields = ['label_name']
